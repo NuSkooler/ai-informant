f01b64f5bb077c0d8280282d961a8e4c71579a32
Bryan D. Ashby <bryan@l33t.codes>
2025-07-17 23:09:05 -0600

    Updates

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::vec;
 
 use clap::Parser;
-use rs_openai::{
-    chat::{ChatCompletionMessageRequestBuilder, CreateChatRequestBuilder, Role},
-    OpenAI,
-};
+use rs_openai::interfaces::chat::ChatCompletionMessageRequestBuilder;
+use rs_openai::interfaces::chat::CreateChatRequestBuilder;
+use rs_openai::interfaces::chat::Role;
+use rs_openai::OpenAI;
 use std::io::{stdout, Write};
-use tokio_stream::{StreamExt};
+use tokio_stream::StreamExt;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -14,7 +14,7 @@ struct Cli {
     prompt: String,
 
     /// Should responses be streamed
-    #[clap(long, default_value="true")]
+    #[clap(long, default_value = "true")]
     stream: bool,
 
     /// The user representing the query
@@ -39,29 +39,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         org_id: cli.api_org,
     });
 
-    if cli.stream == true {
+    if cli.stream {
+        let sys = ChatCompletionMessageRequestBuilder::default()
+            .role(Role::Assistant)
+            .name("Kisin")
+            .content("You are Kisin, the skeletal death god, creator of the underworld. You speak in English, with fragments of Mayan mixed in. You are a ever present, yet invisible host on the system a user is logging into.")
+            .build()?;
+
         let req = CreateChatRequestBuilder::default()
             .model("gpt-3.5-turbo")
-            .messages(vec![ChatCompletionMessageRequestBuilder::default()
-                .role(Role::System)
-                .name(cli.user)
-                .content(cli.prompt)
-                .build()?])
+            .messages(vec![
+                sys,
+                ChatCompletionMessageRequestBuilder::default()
+                    .role(Role::System)
+                    .name(cli.user)
+                    .content(cli.prompt)
+                    .build()?,
+            ])
             .stream(true)
             .build()?;
-        
+
         let mut stream = client.chat().create_with_stream(&req).await?;
 
         let mut lock = stdout().lock();
-    while let Some(response) = stream.next().await {
-        response.unwrap().choices.iter().for_each(|choice| {
-            if let Some(ref content) = choice.delta.content {
-                write!(lock, "{}", content).unwrap();
-            }
-        });
+        while let Some(response) = stream.next().await {
+            response.unwrap().choices.iter().for_each(|choice| {
+                if let Some(ref content) = choice.delta.content {
+                    write!(lock, "{content}").unwrap();
+                }
+            });
 
-        stdout().flush()?;
-    }
+            stdout().flush()?;
+        }
     } else {
         let req = CreateChatRequestBuilder::default()
             .model("gpt-3.5-turbo")
